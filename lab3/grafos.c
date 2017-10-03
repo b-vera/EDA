0e92477ad697f0a6b46bbb2c50c0e91ba365c7d3
Bass Vera <BassVera@Bass-Vera.local>
2017-10-03 14:23:49 -0300

    inicio laboratorio 3

---
+++ lab3/grafos.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct grafo {
+    int vertices;
+    int enlaces;
+    int **Matriz;
+} grafo;
+
+typedef struct enlace {
+    int inicial;
+    int final;
+
+}
